f44fb83da052512b1f6cf0ec4ef077be9da11fa8
MrMime20 <150980767+MrMime20@users.noreply.github.com>
2026-08-02 15:20:40 -0500

    Change ripple background image source
    Updated the image source for the interactive ripple background to a random image from Picsum.

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ const loader = document.getElementById('loader');
 
 const img = new Image();
 img.crossOrigin = 'Anonymous';
-img.src = 'Waterfall.webp';
+// Cover image for the interactive ripple background on the home page
+img.src = 'https://picsum.photos/1000/1000?random=100';
 
 img.onload = () => {
     rebuild();
